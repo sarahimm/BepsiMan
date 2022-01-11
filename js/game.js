adb5c4fa3deb190860336d9be9b6189948b4a910
jojennus25 <jojennus25@my.whitworth.edu>
2022-01-11 11:21:18 -0800

    paralax "working", but refreshing every layer after one has finished moving

--- js/game.js
+++ js/game.js
@@ -1,36 +1,36 @@
 (function($){
     var position = 0;
     var speed = 6;
-    // Use jQuery to get the background element
+
     var $background = $("#background");
     var $background = $("#midground");
     var $background = $("#foreground");
-    var requestAniFra = (function() {
-        if (window.msRequestAnimationFrame) return window.msRequestAnimationFrame;
+
+    var requestAnimFrame = ( function() {
         if (window.requestAnimationFrame) return window.requestAnimationFrame;
         if (window.webkitRequestAnimationFrame) return window.webkitRequestAnimationFrame;
         if (window.mozRequestAnimationFrame) return window.mozRequestAnimationFrame;
         if (window.oRequestAnimationFrame) return window.oRequestAnimationFrame;
-
+        if (window.msRequestAnimationFrame) return window.msRequestAnimationFrame;
         else return function( callback, element ){
-            window.setTimeout(callback, element);
-            };
-            })();
+        window.setTimeout(callback, element);
+        };
+        })();
 
     function draw(){
-        // request draw be called again in 25 milliseconds
-         requestAnimFrame(draw,25);
-        // reset position to 0 once the image has scrolled far enough
-        if ( position < -$background.width() ) {
-        position = 0;
-    }   
-    // Set actual background position
-    $('#background').css('background-position',position *(1 / 12) );
-    $('#midground').css('background-position',position *(1 / 10) );
-    $('#foreground').css('background-position',position  );
-    // Update the background position by the speed
-    position = position - speed;
+        requestAnimFrame(draw,25);
+        if (position < -$background.width() ) {
+            position = 0;
+        }
+
+        $('#background').css('background-position',position*1/12);
+        $('#midground').css('background-position',position*1/6);
+        $('#foreground').css('background-position',position*1/3);
+        position = position - speed;
+
+        if ( position* (1.0/12.0) < -$background.width() ) {
+            position = 0;
+            }
     }
     draw();
-
 })(jQuery);

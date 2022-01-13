0dc14745eb73aeed519c06567846f2b9565cd0aa
Angel Ainembabazi <aainembabazi23@my.whitworth.edu>
2022-01-12 20:51:37 -0800

    bepsiMan object

--- js/bepsi.js
+++ js/bepsi.js
@@ -1,25 +1,10 @@
-//sodaMan object
 
-var BepsiMan = {
-    state : 'right',
-
-    go_left : function(){
-        this.state = 'left'
-    },
-    go_right : function(){
-        this.state = 'right'
-    },
-    display : function(){
-        var elements = "<p>"
-        elements += "BepsiMan going <b>" + this.state;
-        elements += "</b></p>";
-        document.getElementById("output").innerHTML += elements;
-      },       
-};
-
-BepsiMan.display();
-BepsiMan.go_left();
-BepsiMan.display();
-BepsiMan.go_right();
-BepsiMan.display();
-
+class BepsiMan {
+    //Generates a can at the given position
+    constructor(x,y) {
+        this.yPos = y;
+        this.xPos = x;
+        this.height = 80;
+        this.width = 40;
+    }
+}

6dfe2452febe5f68e90ea1760319e7bb95e6bc99
Sarah Immel <sarahim@live.com>
2022-01-13 10:13:31 -0800

    Added jump methods

--- js/Game.js
+++ js/Game.js
@@ -3,8 +3,8 @@ class Game {
     //Initializes background images and starting velocity. 
     //Calls start() when all the images have loaded.
     constructor(){
-        this.Urls = ["img/BackgroundLayer.png","img/MiddleLayer.png","img/FrontLayer.png","img/can.png","img/frame1.png","img/frame2.png","img/frame3.png","img/frame4.png","img/frame5.png","img/frame6.png","img/frame7.png"];
-        let urlNum=11;
+        this.Urls = ["img/BackgroundLayer.png","img/MiddleLayer.png","img/FrontLayer.png","img/can.png","img/frame1.png","img/frame2.png","img/frame3.png","img/frame4.png","img/frame5.png","img/frame6.png","img/frame7.png","img/beginJumpF1.png","img/beginJumpF2.png","img/inAir1.png","img/inAir2.png","img/inAir3.png"];
+        let urlNum=16;
         
         //Position (px) for lefthand side of background images, relative to canvas
         this.backPos = [0,0,0];
@@ -31,7 +31,6 @@ class Game {
 
         //Construct BepsiMan and load his animation images to the object
         this.bepsiMan = new BepsiMan((this.canvas.width * .5 - 100),this.canvas.height - 250);
-        this.runAnim = [];
         for(let i=4; i<11; i++){
             let $img = new Image();
             $img.onload = function(){
@@ -40,6 +39,15 @@ class Game {
                 if(loadedCount===urlNum) start();};
             $img.src = this.Urls[i];
             this.bepsiMan.runAnim.push($img);
+        }
+        for(let i=11; i<16; i++){
+            let $img = new Image();
+            $img.onload = function(){
+                loadedCount++;
+                //Only starts game if all necessary images have loaded
+                if(loadedCount===urlNum) start();};
+            $img.src = this.Urls[i];
+            this.bepsiMan.jumpAnim.push($img);
         }
 
         //Load the can image
@@ -73,6 +81,12 @@ class Game {
                 this.bepsiMan.curFrame = 0;
             this.context.drawImage(this.bepsiMan.runAnim[this.bepsiMan.curFrame],this.bepsiMan.xPos,this.bepsiMan.yPos, this.bepsiMan.width, this.bepsiMan.height);
             this.bepsiMan.curFrame++;  
+        }else{
+            //Draw BepsiMan if jumping (start with transition frames jumpAnim[0],jumpAnim[1], then cycle through jumpAnim[2-4])
+            if(this.bepsiMan.curFrame >= this.bepsiMan.jumpAnim.length)
+                this.bepsiMan.curFrame = 2;
+            this.context.drawImage(this.bepsiMan.jumpAnim[this.bepsiMan.curFrame],this.bepsiMan.xPos,this.bepsiMan.yPos, this.bepsiMan.width, this.bepsiMan.height);
+            this.bepsiMan.curFrame++;
         }
         for (let can of this.cans) {
             this.context.drawImage(this.canIcon,can.xPos,can.yPos, can.width, can.height);
@@ -96,6 +110,9 @@ class Game {
             if(this.cans[i].xPos<=0)
                 this.cans.splice(i, 1);
         }
+        //If bepsiMan is jumping, update his yPos
+        if(this.bepsiMan.inAir)
+            this.bepsiMan.updateHeight();
         if(Math.random() <= this.canProb){
             this.cans.push(new Can(this.canvas.width, Math.floor(Math.random() * (this.canvas.height - 80))));
         }
--- js/bepsi.js
+++ js/bepsi.js
@@ -3,6 +3,7 @@ class BepsiMan {
     //Generates a can at the given position
     constructor(x,y) {
         this.yPos = y;
+        this.yPosOrigin = y;
         this.xPos = x;
         this.height = 200;
         this.width = 200;
@@ -10,5 +11,32 @@ class BepsiMan {
         this.runAnim = [];
         this.jumpAnim = [];
         this.curFrame = 0;
+        this.yVelocity = 0;
+        this.gravity = 4;
+        window.addEventListener("contextmenu",this.highJump.bind(this));
+        window.addEventListener("click",this.lowJump.bind(this));
+    }
+    lowJump(){
+        if(this.inAir===false) {
+            this.inAir=true;
+            this.yVelocity = -40;
+            this.curFrame=0;
+        }   
+    }
+    highJump(e){
+        e.preventDefault();
+        if(this.inAir===false) {
+            this.inAir=true;
+            this.curFrame=0;
+            this.yVelocity=-50;
+        }
+    }
+    updateHeight(){
+        this.yPos += this.yVelocity;
+        this.yVelocity += this.gravity;
+        if(this.yPos >= this.yPosOrigin){
+            this.yPos = this.yPosOrigin;
+            this.inAir = false;
+        }
     }
 }

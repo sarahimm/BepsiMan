f54d347802bf0ee1b58a575ebe055af7c22d7bd3
Angel Ainembabazi <aainembabazi23@my.whitworth.edu>
2022-01-13 10:03:41 -0800

    ..

--- js/bepsiMan.js
+++
@@ -1,9 +0,0 @@
-class BepsiMan {
-    //Generates bepsiMan at the given position
-    constructor(x,y) {
-        this.yPos = y;
-        this.xPos = x;
-        this.height = 80;
-        this.width = 40;
-    }
-}
